cf9122582d3156f70b7a471efa3d1124707892d6
Tibo Vanheule <tibo@tibovanheule.space>
2020-03-25 16:06:31 +0100

    test

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ const fs = require('fs'),
     //sharp = require('sharp'),
     keptEvents = ["SharePresentationEvent", "GotoSlideEvent", "StartDeskShareEvent"],
     prompt_attributes = [
-        {
-            name: 'path',
-            description: 'path to bongo download.'
-        },
-        {
-            name: 'chat',
-            description: "type yes to include chat in conversion."
-        }
+        {name: 'path', description: 'path to bongo download.'},
+        {name: 'chat', description: "type yes to include chat in conversion."},
+        {name: 'cache', description: "Overwrite cache if exist?"}
     ];
 
 prompt.start();
@@ -24,8 +19,10 @@ prompt.get(prompt_attributes, (err, result) => {
         console.log(err);
         return 1;
     } else {
-        let parser = new xml2js.Parser(), chat = result.chat.toLowerCase() === "yes",
-            path = "C:\\Users\\seven\\Desktop\\meetingFiles";
+        let parser = new xml2js.Parser(),
+            chat = result.chat.toLowerCase() === "yes",
+            path = "C:\\Users\\seven\\Desktop\\meetingFiles",
+            cache = result.cache.toLowerCase() === "yes";
 
         //conversie xml to json
         console.log("reading xml-data from:" + path + "/events.xml");
@@ -39,27 +36,35 @@ prompt.get(prompt_attributes, (err, result) => {
                     let name = json.meeting[0]['$'].name;
                     let events = json.event;
                     events = events.filter(item => keptEvents.includes(item['$'].eventname));
-                    resize_images(events, path).then((images) => {
+                    resize_images(events, path).then(async (images) => {
                         console.log("Done reading\nPreparing video conversion, this could take a while");
-                        let videoOptions = {
-                            transition: false,
-                        };
-                        //slicen op 50 tis anders te veel om te testen
-                        videoshow(images.slice(0,5),videoOptions)
-                            .save(`${path}/tempPresentation2.mp4`)
-                            .on('start', (cli) => console.log(cli))
-                            .on('error', console.log)
-                            .on('progress', console.log)
-                            .on('end', () => {
-                                let command = ffmpeg_command();
-                                command.addOption('-c copy');
-                                command.input(`${path}/tempPresentation2.mp4`);
-                                //command.input(``);
-                                //onderstaand commando is voor later (chat)
-                                //command.addOption(`-vf "movie=dbz120.mp4 [a]; movie=dbz121.mp4 [b]; [in][a] overlay=0:32 [c]; [c][b] overlay=0:448`);
-                                command.addOption(`-vf "movie=${path}/__157180_6828003742b85dddddfb4e14279dc527.mp4 [a]; [in][a] overlay=0:32 [c]`);
-                                command.save(`${path}/${name}.mp4`);
-                            })
+
+                        let splitimmages = [], step = 10;
+                        while (images.length !== 0) {
+                            if (images.length < step) {
+                                splitimmages.push(images.splice(0, images.length))
+                            } else {
+                                splitimmages.push(images.splice(0, step))
+                            }
+                        }
+                        let video = [];
+                        for (let i = 0; i < splitimmages.length; i++) {
+                            if (cache || !fs.existsSync(`${path}/tempp${i}.mp4`)) {
+                                video.push(await images_to_viceo(splitimmages[i], path, i));
+                            }
+                            console.log(i / splitimmages.length)
+                        }
+                        console.log(video);
+
+                        let concat_video = video.reduce((result,input)=>result.input(input),ffmpeg_command());
+                        concat_video.addOption('-c copy');
+                        concat_video.mergeToFile(`${path}/tempPresentation2.mp4`);
+
+                        let command = ffmpeg_command();
+                        command.addOption('-c copy');
+                        command.input(`${path}/tempPresentation2.mp4`).inputOption('-hwaccel auto');
+                        command.addOption(`--vf "movie=${path}/__157180_6828003742b85dddddfb4e14279dc527.mp4 [a]; [in][a] overlay=0:32 [c]`);
+                        command.save(`${path}/${name}.mp4`);
 
 
                         //let chainedInputs = inputlist.reduce((result, inputItem) => result.addInput(inputItem), ffmpeg_command());
@@ -98,8 +103,26 @@ function get_all_unique_event_names(events) {
     console.log(unique);
 }
 
+function images_to_viceo(images, path, i) {
+    let videoOptions = {
+        transition: false,
+        hwaccel: "auto",
+
+    };
+    return new Promise((resolve, reject) => {
+        videoshow(images, videoOptions)
+            .option('-s 8')
+            .option('-preset ultrafast')
+            .save(`${path}/tempp${i}.mp4`)
+            .on('start', (v) => console.log(v, `\ntempp${i}`))
+            .on('error', reject)
+            .on('end', () =>
+                resolve(`${path}/tempp${i}.mp4`))
+    })
+}
+
 async function resize_images(events, path) {
-    let res, picture = "", images = [];
+    let res, picture = "", images = [], first = events[0]['$'].timestamp, times = [];
     for (let i = 0; i < events.length; i++) {
         let item = events[i];
 
@@ -117,11 +140,16 @@ async function resize_images(events, path) {
                                     }).toFile(image, () => console.log("resized image: " + image));
                                 });
                 */
+
+                let time = ((find_next_slide(events, i)) - (item['$'].timestamp)) / 1000;
+                times.push(time);
+
                 //find_next_slide(events,i)-item['$'].timestamp
-                images.push({path: image, loop: 1})
+                images.push({path: image, loop: time})
             }
         }
     }
+    console.log(times);
     return images;
 }
 
